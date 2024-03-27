f997e00a41555a90e4108630919d46ff0ae8feb5
Wombatlord <jordan.stone0@gmail.com>
2024-03-27 21:09:54 +0000

    text creation with author post request

--- src/services/post.rs
+++ src/services/post.rs
@@ -41,9 +41,15 @@ struct CreateText {
 
 #[post("/create/text")]
 pub async fn create_text(state: Data<AppState>, create_text: Json<CreateText>) -> impl Responder {
+    // curl -H 'Content-Type: application/json' -d '[{"text_type_id": 1, "author_id": 0, "title": "Wuthering Heights", "published": 1847, "metadata": {"genre_tags": ["Gothic"]}}, {"first_name": "Emily", "last_name":"Bronte"}]' -X POST http://localhost:8080/create/text
+
     println!("I'm tryin' chief");
     let sql = "SELECT author_id, first_name, last_name FROM authors WHERE authors.first_name = $1";
     println!("{create_text:?}");
+
+    let Ok(mut txn) = state.db.begin().await else {
+        return HttpResponse::InternalServerError().json("Failed to create transaction");
+    };
 
     if let Ok(Some(author)) = sqlx::query_as::<_, Author>(sql)
         .bind(&create_text.author.first_name)
@@ -54,7 +60,7 @@ pub async fn create_text(state: Data<AppState>, create_text: Json<CreateText>) -
         let author_id = author.author_id.unwrap();
 
         let text_sql = "INSERT INTO texts (text_type_id, author_id, title, published, metadata) VALUES ($1, $2, $3, $4, $5) RETURNING text_type_id, author_id, title, published, metadata";
-        
+
         let ser = json!(&create_text.text.metadata);
         match sqlx::query_as::<_, Text>(text_sql)
             .bind(&create_text.text.text_type_id)
@@ -62,13 +68,60 @@ pub async fn create_text(state: Data<AppState>, create_text: Json<CreateText>) -
             .bind(&create_text.text.title)
             .bind(&create_text.text.published)
             .bind(&ser)
-            .fetch_one(&state.db)
+            .fetch_one(&mut *txn)
             .await
         {
-            Ok(text) => HttpResponse::Ok().json(text),
-            Err(e) => {eprintln!("{e}");HttpResponse::InternalServerError().json("Failed to create new text")},
+            Ok(text) => {
+                let Ok(_) = txn.commit().await else {
+                    return HttpResponse::InternalServerError()
+                        .json("Failed to commit transaction");
+                };
+                HttpResponse::Ok().json(text)
+            }
+            Err(e) => {
+                eprintln!("{e}");
+                HttpResponse::InternalServerError().json("Failed to create new text")
+            }
         }
     } else {
-        HttpResponse::InternalServerError().json("Author does not yet exist.")
+        let author_sql = "INSERT INTO authors (first_name, last_name) VALUES ($1, $2) RETURNING authors.author_id, authors.first_name, authors.last_name";
+        match sqlx::query_as::<_, Author>(author_sql)
+            .bind(&create_text.author.first_name)
+            .bind(&create_text.author.last_name)
+            .fetch_one(&mut *txn)
+            .await
+        {
+            Ok(author) => {
+                let text_sql = "INSERT INTO texts (text_type_id, author_id, title, published, metadata) VALUES ($1, $2, $3, $4, $5) RETURNING text_type_id, author_id, title, published, metadata";
+
+                let ser = json!(&create_text.text.metadata);
+                match sqlx::query_as::<_, Text>(text_sql)
+                    .bind(&create_text.text.text_type_id)
+                    .bind(&author.author_id)
+                    .bind(&create_text.text.title)
+                    .bind(&create_text.text.published)
+                    .bind(&ser)
+                    .fetch_one(&mut *txn)
+                    .await
+                {
+                    Ok(text) => {
+                        let Ok(_) = txn.commit().await else {
+                            return HttpResponse::InternalServerError()
+                                .json("Failed to commit transaction");
+                        };
+                        return HttpResponse::Ok().json(text);
+                    }
+                    Err(e) => {
+                        eprintln!("{e}");
+                        return HttpResponse::InternalServerError()
+                            .json("Failed to create new text");
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("{e}");
+                return HttpResponse::InternalServerError().json("Failed to create new author");
+            }
+        }
     }
 }
