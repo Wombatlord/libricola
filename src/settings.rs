7b770dc2f1062826a77e2633853224c0e0626864
Wombatlord <jordan.stone0@gmail.com>
2024-04-04 03:16:36 +0100

    application settings from config files

---
+++ src/settings.rs
@@ -0,0 +1,82 @@
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct Settings {
+    pub application: ApplicationSettings,
+    pub debug: bool,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
+    pub base_url: String,
+    pub protocol: String,
+}
+
+pub enum Environment {
+    Development,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Development => "development",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "development" => Ok(Environment::Development),
+            "production" => Ok(Environment::Production),
+            other => Err(format!(
+                "{} is not a supported environment. Use development or production.",
+                other
+            )),
+        }
+    }
+}
+
+/// Multipurpose function that helps detect the current environment the application
+/// is running using the `APP_ENVIRONMENT` environment variable.
+///
+/// \`\`\`
+/// APP_ENVIRONMENT = development | production.
+/// \`\`\`
+///
+/// After detection, it loads the appropriate .yaml file
+/// then it loads the environment variable that overrides whatever is set in the .yaml file.
+/// For this to work, you the environment variable MUST be in uppercase and starts with `APP`,
+/// a `_` separator then the category of settings,
+/// followed by `__` separator,  and then the variable, e.g.
+/// `APP__APPLICATION_PORT=5001` for `port` to be set as `5001`
+
+pub fn get_settings() -> Result<Settings, config::ConfigError> {
+    let base_path = std::env::current_dir().expect("Failed to determine current directory");
+    let settings_dir = base_path.join("settings");
+
+    //detect running environment
+    //default to development if unspecified
+
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "development".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT.");
+    let environment_filename = format!("{}.yaml", environment.as_str());
+    let settings = config::Config::builder()
+        .add_source(config::File::from(settings_dir.join("base.yaml")))
+        .add_source(config::File::from(settings_dir.join(environment_filename)))
+
+    // Add in settings from environment variables (with a prefix of APP and '__' as separator)
+    // E.g. `APP_APPLICATION__PORT=5001 would set `Settings.application.port`
+        .add_source(config::Environment::with_prefix("APP").prefix_separator("_").separator("__"))
+        .build()?;
+
+    settings.try_deserialize::<Settings>()
+}
